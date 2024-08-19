cf4fe6480d1fbb4793be5c1a279c4680e1b5a8dd
Peter Omollo <omollpeter@gmail.com>
2024-08-19 09:51:06 +0300

    Fix 0-simple_helper

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 This module contains a function that returns a tuple containing start
-index and end index corresponding to range of indexes to return in a 
+index and end index corresponding to range of indexes to return in a
 list
 """
 
