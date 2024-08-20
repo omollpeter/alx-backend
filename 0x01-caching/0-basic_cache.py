976feb75e58fa4a427d4f5c94cb42e2705ac9e66
Peter Omollo <omollpeter@gmail.com>
2024-08-20 15:18:01 +0300

    Create a class BasicCache that inherits from BaseCaching and is a caching system

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+This module contains BasicCache class that inherits from BaseCaching
+"""
+
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def __init__(self):
+        """
+        Initializes instance attributes
+        """
+        super().__init__()
+    
+    def put(self, key, item):
+        """
+        For inserting values to dict like cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieves data from the cache using the key
+        """
+        if key:
+            return self.cache_data.get(key)
+        return None
---
+++ 0x01-caching/README.md
@@ -0,0 +1,1 @@
+### This folder contains projects involving cache replacement policies
