87624ac73600c053b1a3d8ec03ed7893c40dc1cb
Peter Omollo <omollpeter@gmail.com>
2024-08-21 13:14:27 +0300

    Create a class MRUCache that inherits from BaseCaching and is a caching system

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""
+This module contains MRUCache class that inherits from BaseCaching
+The class defines implementation for MRU caching replacement policy
+"""
+
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    Defines MRUCache class
+    """
+
+    def __init__(self):
+        """
+        Initializes instance attributes
+        """
+        super().__init__()
+        self.trkr = []
+
+    def put(self, key, item):
+        """
+        Adds data to a dict like cache
+        Replaces the oldest data if cache is full
+        """
+        if key and item:
+            if key in self.cache_data.keys():
+                idx = self.trkr.index(key)
+                self.trkr[idx], self.trkr[3] = self.trkr[3], self.trkr[idx]
+                self.cache_data[key] = item
+                return
+            if len(self.cache_data) < self.MAX_ITEMS:
+                self.trkr.append(key)
+                self.cache_data[key] = item
+            else:
+                discard = self.trkr.pop(3)
+                print("DISCARD:", discard)
+                del self.cache_data[discard]
+
+                self.trkr.append(key)
+                self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieves data from the cache using the key
+        """
+        if key:
+            if key in self.cache_data.keys():
+                idx = self.trkr.index(key)
+                self.trkr[idx], self.trkr[3] = self.trkr[3], self.trkr[idx]
+            return self.cache_data.get(key)
+        return None
---
+++ 0x01-caching/test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+a_dict = {"a": 1, "b": 2, "c": 3, "d": 4}
+
+s_dict = dict(sorted(a_dict.items(), key=lambda item: item[1], reverse=True))
+print(s_dict.items())
