9c0b9c55b22c402e9f891e8e3516903cbe7ee941
Peter Omollo <omollpeter@gmail.com>
2024-08-22 05:53:39 +0300

    Create a class LFUCache that inherits from BaseCaching and is a caching system:

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -31,17 +31,29 @@ class LFUCache(BaseCaching):
                 idx = self.trkr.index(key)
                 self.trkr.remove(key)
                 self.trkr.append(key)
+                self.frequency[key] += self.trkr.index(key)
                 self.cache_data[key] = item
                 return
             if len(self.cache_data) < self.MAX_ITEMS:
                 self.trkr.append(key)
+                self.frequency[key] = 0 + self.trkr.index(key)
                 self.cache_data[key] = item
             else:
-                discard = self.trkr.pop(0)
+                s_freq = dict(
+                    sorted(
+                        self.frequency.items(),
+                        key=lambda item: item[1],
+                        reverse=True
+                    )
+                )
+
+                discard = s_freq.popitem()[0]
+                self.frequency.pop(discard)
                 print("DISCARD:", discard)
                 del self.cache_data[discard]
 
                 self.trkr.append(key)
+                self.frequency[key] = 0 + self.trkr.index(key)
                 self.cache_data[key] = item
 
     def get(self, key):
@@ -53,5 +65,7 @@ class LFUCache(BaseCaching):
                 idx = self.trkr.index(key)
                 self.trkr.remove(key)
                 self.trkr.append(key)
+                self.frequency[key] += self.trkr.index(key)
+
             return self.cache_data.get(key)
         return None
--- 0x01-caching/test.py
+++ 0x01-caching/test.py
@@ -3,4 +3,4 @@
 a_dict = {"a": 1, "b": 2, "c": 3, "d": 4}
 
 s_dict = dict(sorted(a_dict.items(), key=lambda item: item[1], reverse=True))
-print(s_dict.items())
+print(s_dict.popitem()[0])
