6b2c44dafc051afe67e8446b5e3c5fa270cd172c
Peter Omollo <omollpeter@gmail.com>
2024-08-28 11:39:43 +0300

    Fix 3-index.html

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -40,7 +40,7 @@ def index():
     """
     View function for the the index page
     """
-    return render_template("2-index.html")
+    return render_template("3-index.html")
 
 
 if __name__ == "__main__":
