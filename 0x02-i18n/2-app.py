4123dfa3af3bdbc6e8ea876ad2605f34f1f69f0e
Peter Omollo <omollpeter@gmail.com>
2024-08-28 10:49:21 +0300

    Fix pycodestyle

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -23,9 +23,14 @@ app.config.from_object(Config)
 
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale():
+    """
+    Matches the best language to be used in the application
+    """
     return request.accept_languages.best_match(app.config["LANGUAGES"])
+
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index():
