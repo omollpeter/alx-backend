65a892d08b1fefb1a74fd49076aa21bbc258e898
Peter Omollo <omollpeter@gmail.com>
2024-09-26 05:52:09 +0300

    Create a processor for multiple jobs

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,23 @@
+import kue from "kue";
+
+const blackListed = ["4153518780", "4153518781"];
+
+const sendNotification = (phoneNumber, message, job, done) => {
+    job.progress(0, 100);
+
+    if (blackListed.includes(phoneNumber)) {
+        return done(new Error(`Phone number ${phoneNumber} is blacklisted`))
+    }
+
+    job.progress(50, 100);
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`)
+    done();
+}
+
+const queue = kue.createQueue()
+
+queue.process(
+    "push_notification_code_2", 2, (job, done) => {
+        sendNotification(job.data.phoneNumber, job.data.message, job, done);
+    }
+)
